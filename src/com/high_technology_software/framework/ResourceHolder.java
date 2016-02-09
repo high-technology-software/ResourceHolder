f1444f77f7a2499f5c27dc5ce7f7cbbc4388055b
foxtrot-developer <foxtrot-developer@users.noreply.github.com>
2016-02-09 15:03:21 +0100

    Force resource files

--- src/com/high_technology_software/framework/ResourceHolder.java
+++ src/com/high_technology_software/framework/ResourceHolder.java
@@ -5,27 +5,62 @@ import java.util.ResourceBundle;
 
 public class ResourceHolder {
 
-	private ResourceBundle mResource;
+	private Properties mProperties;
 	private final String ERROR_NO_KEY = "<¿ Key: '{0}' doesn't exist ?>";
+	private final String ERROR_NO_FOUND_FILE = "FILE_NOT_FOUND";
 
-	public ResourceHolder(String filename) {
-		mResource = ResourceBundle.getBundle(filename);
+	public ResourceHolder(String basename) {
+		InputStream file = choosePropertiesFile(basename, null, null);
+		initialization(file);
 	}
 
-	public ResourceHolder(String filename, Locale locale) {
-		mResource = ResourceBundle.getBundle(filename, locale);
+	public ResourceHolder(String basename, Locale locale) {
+		InputStream file = choosePropertiesFile(basename, locale.getLanguage(), locale.getCountry());
+		initialization(file);
+	}
+
+	private void initialization(InputStream file) {
+		mProperties = new Properties();
+		try {
+			mProperties.load(file);
+		} catch (IOException e) {
+			System.out.println("ResourceHolder >> " + ERROR_NO_FOUND_FILE);
+		}
 	}
 
 	public String getValue(String key) {
 		String value;
-		
+
 		try {
-			value = mResource.getString(key);
+			value = String.valueOf(mProperties.get(key));
 		} catch (Exception e) {
 			value = ERROR_NO_KEY.replace("{0}", key);
 		}
-		
+
 		return value;
+	}
+
+	private InputStream choosePropertiesFile(String basename, String language, String country) {
+		InputStream result = null;
+		int intent = (language == null ? 1 : (country == null ? 2 : 3));
+
+		while (intent > 0 && result == null) {
+			StringBuilder filename = new StringBuilder();
+
+			switch (intent) {
+				case 3:
+					filename = new StringBuilder("_").append(country);
+				case 2:
+					filename = new StringBuilder("_").append(language).append(filename);
+				case 1:
+					filename = new StringBuilder(basename).append(filename).append(".properties");
+			}
+
+			result = getClass().getClassLoader().getResourceAsStream(filename.toString());
+			intent--;
+		}
+
+		return result;
 	}
 
 }
